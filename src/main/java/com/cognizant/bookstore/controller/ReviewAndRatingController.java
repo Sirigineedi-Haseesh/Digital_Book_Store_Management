070fc6df9c2b91ff4e48a75852ec77e54c136fbf
SOWMYA-ACHINA <Achina.Sowmya@cognizant.com>
2025-04-06 15:35:59 +0530

    Your commit message

---
+++ src/main/java/com/cognizant/bookstore/controller/ReviewAndRatingController.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reviews-and-ratings")
+public class ReviewAndRatingController {
+
+    @Autowired
+    private ReviewAndRatingService service;
+
+    @PostMapping
+    public ReviewAndRating addReviewAndRating(@RequestBody ReviewAndRating reviewAndRating) {
+        return service.addReviewAndRating(reviewAndRating);
+    }
+
+    @GetMapping
+    public List<ReviewAndRating> getAllReviewsAndRatings() {
+        return service.getAllReviewsAndRatings();
+    }
+
+    @GetMapping("/{id}")
+    public ReviewAndRating getReviewAndRatingById(@PathVariable int id) {
+        return service.getReviewAndRatingById(id);
+    }
+
+    @PutMapping
+    public ReviewAndRating updateReviewAndRating(@RequestBody ReviewAndRating reviewAndRating) {
+        return service.updateReviewAndRating(reviewAndRating);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteReviewAndRating(@PathVariable int id) {
+        service.deleteReviewAndRating(id);
+        return "Review and rating deleted successfully!";
+    }
+}
